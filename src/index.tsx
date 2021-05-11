84b2e2ff609f9ab8e1057e1063b01543a02fcd0c
eddientim <edward11r@yahoo.co.uk>
2021-05-11 23:08:01 +0100

    adds component to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React, {ReactElement} from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
+import {Router} from 'react-router-dom';
+import {createBrowserHistory} from "history";
+import App from "./App";
 
+const history = createBrowserHistory();
 
 const Index: React.FunctionComponent = (): ReactElement => {
 
     return (
-        <Router>
-
+        <Router history={history}>
+            <App/>
         </Router>
+
     );
 }
 
